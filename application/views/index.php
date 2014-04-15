6a493404ee483f4ad9f35c2fd14ee26d777f90d0
mhintzke <matt.hintzke@email.wsu.edu>
2014-04-14 19:40:00 -0700

    Made some config changes

---
+++ application/views/index.php
@@ -0,0 +1,12 @@
+<html>
+    <head>
+        <title>Women's Transit</title>
+        <!--
+            Here we need to add all our content... css/js
+        -->
+    </head>
+    <body>
+        <h1>Welcome to Women's Transit!</h1>
+    </body>
+    
+</html>
