1a3e45cdd48e44f19d7a9a203ccb64abfa452536
mhintzke <matt.hintzke@email.wsu.edu>
2014-04-15 23:37:05 -0700

    Created an index page

--- application/views/home/index.php
+++ application/views/home/index.php
@@ -1,4 +1,60 @@
-<h1>Login</h1>
-<form action='login.php' method="POST">
-    
-</form>
+
+
+<section id="title-tab" class="section">
+    <div class="center">
+        <h1 id="main-title">WSU Women's Transit</h1>
+    </div>
+</section>
+
+<section id="login-tab" class="section">
+    <div class="left">
+        
+        <h1>Login</h1>
+
+        <form id="form-login" role="form" action='login.php' method="POST">
+            <div class="form-group">
+                <label for="login-user">Email</label>
+                <input type="email" id="login-user" class="form-control input" placeholder="Enter email" />
+            </div>
+
+            <div class="form-group">
+                <label for="login-pass">Password</label>
+                <input type="password" id="login-pass" class="form-control input" placeholder="Enter password" />
+            </div>
+
+            <button id="login-btn" class="btn btn-primary">Login</button>
+        </form>
+        
+        <div id="register-wrap">
+            <h1 class="register-btn font-racing">Or Register</h1>
+        </div>
+    </div>
+</section>
+
+<section id="app-tab" class="section">
+    <div class="center">
+          <h1 style="text-align:left; border-bottom:2px solid #000; padding:8px 0;">Welcome</h1>
+        <h3 class="info-header">Welcome to the new WSU Women's Transit online web app! Register for quick access to a Women's Transit volunteer ride directly from your mobile device</h3>
+    </div>
+</section>
+
+<section id="about-tab" class="section">
+    <div class="center">
+        <h1 style="text-align:left; border-bottom:2px solid #000; padding:8px 0;">About</h1>
+        <h3 class="info-header">Women's Transit is a safety service for WSU students and the Pullman community, provided by the WSU Coalition for Women Students and supported by the WSU Women's Resource Center, the CougParents program, and the WSU Center for Civic Engagement. We offer free rides almost anywhere within the Pullman city limits and walking escorts on the WSU campus. We also provide valuable volunteer opportunities to nearly 200 students each semester.</h3>
+    </div>
+</section>
+
+<section id="join-tab" class="section">
+    <div class="center">
+        <h1 style="text-align:left; border-bottom:2px solid #000; padding:8px 0;">Join Us</h1>
+        <h3 class="info-header">Women's Transit is made up by a number of awesome volunteers that provide safe and speedy transportation.</h2>
+        <br />
+        <h3 class="info-header">We are currently accepting volunteer applications. Please sign up now!</h2>
+        
+        <div id="volunteer">
+            <a href="http://women.wsu.edu/womens-transit/volunteer/">Sign Up!</a>
+        </div>
+    </div>
+</section>
+
--- application/views/layout_main.php
+++ application/views/layout_main.php
@@ -2,16 +2,154 @@
     <head>
         <meta http-equiv="content-type" content="text/html;charset=utf-8" />
         <title>Women's Transit</title>
+        <!--<script src="https://cdnjs.cloudflare.com/ajax/libs/normalize/3.0.1/normalize.min.css" type="text/javascript" />-->
         <link rel="stylesheet" href="/CS483-Final/content/bootstrap/css/bootstrap-responsive.css" type="text/css" />
         <link rel="stylesheet" href="/CS483-Final/content/bootstrap/css/bootstrap.css" type="text/css" />
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.0/jquery.min.js" type="text/javascript"></script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.1.1/js/bootstrap.min.js" type="text/javascript"></script>
+       <link href='http://fonts.googleapis.com/css?family=Racing+Sans+One' rel='stylesheet' type='text/css'>
+        <style type="text/css">
+            /* Global elements */
+            html, body {
+                width:100%;
+                height:100%;
+                margin:0;
+                padding:0;
+               
+            }
+            #wrap {
+                min-width:100%;
+                position: relative;
+                margin:0 auto;
+                background:#fff;
+            }
+            .font-racing {
+                font-family: 'Racing Sans One', cursive;
+            }
+            a:hover {
+                text-decoration: none;
+            }
+            
+            
+            /* Navigation */
+            #nav-bar {
+                background:#000;
+                width:100%;
+                position:fixed;
+                left:0;
+                min-width:960px;
+                z-index: 9999;
+            }
+            #nav-bar-list {
+                list-style: none;
+                position: relative;
+                margin:0 auto;
+            }
+            .nav-item {
+                float:left;
+                line-height: 40px;
+                text-align: center;
+                padding:10px 3px;
+            }
+            .nav-item > a {
+             
+                color:#fff;
+            }
+            
+            /* Content */
+            #content {
+                  top:60px;
+                  position:relative;
+            }
+            .input {
+                padding:8px !important;
+                font:inherit;
+                height:auto !important;
+                width:300px;
+            }
+            label {
+                font-size:20px;
+            }
+            
+            /* Tabs */
+            .section {
+                padding:25px;
+            }
+            .section:nth-child(odd){
+                background:#3FE8A4;
+            }
+            .center {
+                max-width: 960px;
+                position:relative;
+                text-align:center;
+                margin:0 auto;
+            }
+            .left {
+                max-width: 960px;
+                position:relative;
+                text-align:left;
+                margin:0 auto;
+            }
+            .right {
+                max-width: 960px;
+                position:relative;
+                text-align:right;
+                margin:0 auto;
+            }
+            #main-title {
+                font-size:72px;
+                font-family: 'Racing Sans One', cursive;
+                padding:20px 0;
+            }
+            #login-tab {
+                background:#ff;
+                color:#000;
+            }
+            #form-login {
+                width:250px;
+            }
+            #title-tab {
+             
+            }
+            #about-tab {
+          
+            }
+            #register-wrap {
+                border-radius: 25px;
+                background:#3FE8A4;
+                position: absolute;
+                top: 60px;
+                left: 500px;
+                color: #fff;
+            }
+            #register-wrap:hover {
+                background:#000;
+                color:#fff;
+                cursor:pointer;
+            }
+            .register-btn {
+                padding:25px;
+                font-size:60px;
+            }
+            .info-header {
+                text-align:justify;
+            }
+            #volunteer {
+                padding:50px;
+            }
+            #volunteer > a {
+                font-size: 54px;
+                color: #3FE8A4;
+                background: #fff;
+                padding: 20px;
+                margin-top: 50px;
+            }
+            
+        </style>
     </head>
 <body>
     <div id="wrap">
         <nav id="nav-bar">
             <ul id="nav-bar-list">
-                <li class="nav-item"><a href="<?php echo site_url('home/index'); ?>">Home</a></li>
+                <li class="nav-item"><a href="<?php echo site_url('/'); ?>">Home</a></li>
                 <!-- add more links as needed.. for now, we will not check for admin -->
             </ul>
         </nav>
@@ -23,6 +161,7 @@
         </footer>
     </div>
       
-
+     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.0/jquery.min.js" type="text/javascript"></script>
+     <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.1.1/js/bootstrap.min.js" type="text/javascript"></script>
 </body>
 </html>
